57e3e30cb121e9e2a2f4993758a92c580f1a018f
Your Name <your-email@example.com>
2025-09-25 14:14:26 +0530

    init: created ERMS backend project

---
+++ src/main/java/com/example/demo/ErmsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErmsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErmsBackendApplication.class, args);
+	}
+
+}
